c3f844221108d8752363bccc71743802b3d665c6
won <alyduho984530@gmail.com>
2023-11-27 12:55:58 +0900

    [ADD] MainScreen

--- lib/Screens/MainScreen.dart
+++ lib/Screens/MainScreen.dart
@@ -6,32 +6,59 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            color: Color.fromARGB(255, 57, 59, 58),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            height: 100,
-            child: Container(
-              height: 100,
-              color: Colors.white,
-            ),
-          ),
-          Positioned(
-            child: Text(
-              "안녕하세요, 최원입니다-!",
-              style: TextStyle(
-                color: Colors.white,
+        backgroundColor: Colors.deepPurple.shade900,
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.black,
+                              Colors.white,
+                              Colors.black,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "안녕하세요? 포트폴리오 입니다",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 5,
+                                offset: Offset(0, 0),
+                                color: Colors.black,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+              Container(
+                height: 550,
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(255, 11, 4, 42)),
+              )
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: "Won's Portfolio",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
