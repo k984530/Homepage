9801be508a44ba0fb38508a4e2e2f1b9d3c071e6
won <alyduho984530@gmail.com>
2023-11-28 11:09:20 +0900

    [REMOVE] MAC, WINDOW

--- lib/Screens/Components/FlipPhone.dart
+++ lib/Screens/Components/FlipPhone.dart
@@ -33,24 +33,27 @@ class _FilpPhoneWidgetState extends State<FilpPhoneWidget>
       });
   }
 
+  double Phone_Width = 240;
+  double Phone_Height = 400;
   @override
   Widget build(BuildContext context) {
     return Transform(
       alignment: FractionalOffset.bottomRight,
       transform: Matrix4.identity()
-        ..rotate(vector.Vector3(2, 3, 0), pi * _animation.value),
+        ..rotate(vector.Vector3(Phone_Width, Phone_Height, 0),
+            pi * _animation.value),
       // ..rotateY(pi * _animation.value),
       // ..rotateX(pi * 30 / 180)
       child: Card(
           child: _animation.value <= 0.5
               ? Container(
                   color: Colors.blue,
-                  width: 240,
-                  height: 360,
+                  width: Phone_Width,
+                  height: Phone_Height,
                 )
               : Container(
-                  width: 240,
-                  height: 360,
+                  width: Phone_Width,
+                  height: Phone_Height,
                   color: Colors.green,
                 )),
     );
