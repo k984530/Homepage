d00e945985aaae65a874ee11859b61d8237353de
won <alyduho984530@gmail.com>
2023-12-12 18:38:43 +0900

    save

--- lib/Screens/First/ProfileScreen.dart
+++ lib/Screens/First/ProfileScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterweb/Data/HorIndex.dart';
 import 'package:flutterweb/Screens/First/Components/History.dart';
